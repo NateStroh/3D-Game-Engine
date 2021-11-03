af1ac80bd789598655e1e368445a578f94739756
Nate Strohmyer <nate.stroh@live.com>
2021-11-03 00:17:23 -0600

    Getting a start on definitions for 2 main classes

--- Engine/EntityComponentSystem/Component.h
+++ Engine/EntityComponentSystem/Component.h
@@ -2,4 +2,19 @@
 #define EAE6320_COMPONENT_H
 
 
+namespace eae6320
+{
+	namespace ECS {
+		class Component
+		{
+		private:
+
+		public:
+			virtual void Update(const float i_elapsedSecondCount_systemTime, const float i_elapsedSecondCount_sinceLastSimulationUpdate);
+
+		};
+	}
+}
+
+
 #endif
--- Engine/EntityComponentSystem/EntityComponentSystem.h
+++ Engine/EntityComponentSystem/EntityComponentSystem.h
@@ -1,5 +1,27 @@
 #ifndef EAE6320_ENTITYCOMPONENTSYSTEM_H
 #define EAE6320_ENTITYCOMPONENTSYSTEM_H
 
+#include <Engine/Results/Results.h>
+
+#include "Component.h"
+
+namespace eae6320
+{
+	namespace ECS {
+		class EntityComponentSystem
+		{
+		private:
+			//Component m_allComponents[];
+		public:
+			cResult Initialization(const unsigned int i_maxComponents);
+			void Update(const float i_elapsedSecondCount_systemTime, const float i_elapsedSecondCount_sinceLastSimulationUpdate);
+			cResult CleanUp();
+			cResult AddComponent();
+			cResult RemoveComponent();
+			Component* GetComponents(unsigned int i_ID);
+		};
+	}
+}
+
 
 #endif
