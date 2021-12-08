1b5c404868079ebc2aca457b0e7d47ff5094fb89
Nate Strohmyer <nate.stroh@live.com>
2021-12-08 11:49:53 -0700

    adding new project

---
+++ Asteroids_/Content/Meshes/SquareGeometry.lua
@@ -0,0 +1,48 @@
+return {
+	Vertices = {
+		{
+			Position = {0,0,0},
+			Color = {
+				Red = 0,
+				Blue = 1,
+				Green = 0,
+				Alpha = 1,
+			},
+			--Normal = {0,0,0},
+		},
+		{
+			Position = {0,1,0},
+			Color = {
+				Red = 0,
+				Blue = 1,
+				Green = 0,
+				Alpha = 1,
+			},
+			--Normal = {0,0,0},
+		},
+		{
+			Position = {1,1,0},
+			Color = {
+				Red = 0,
+				Blue = 0,
+				Green = 0,
+				Alpha = 1,
+			},
+			--Normal = {0,0,0},
+		},
+		{
+			Position = {1,0,0},
+			Color = {
+				Red = 1,
+				Blue = 1,
+				Green = 0,
+				Alpha = 1,
+			},
+			--Normal = {0,0,0},
+		},
+	},
+	Indices = {
+		0,1,2,
+		0,2,3,
+	},
+}
